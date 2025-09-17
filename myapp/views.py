c8795247fcb84ed931cced325758dcfc2e1636e8
zahidhr99 <zahidhr99@gmail.com>
2025-09-17 20:37:31 +0600

    django template

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,23 @@ def home(request):
     return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+
+    data = {
+        'title': 'About Us',
+        'content': 'Welcome to the About Page',
+        'number': 423.123123123,
+        'list_items': ['Item 1', 'Item 2', 'Item 3'],
+        'boolean': True,
+        'list_dicts': [
+            {'name': 'Alice', 'age': 30},
+            {'name': 'Bob', 'age': 25},
+            {'name': 'Charlie', 'age': 35}
+        ],
+        'mylist': [1, 2, 3, 4, 5],
+        'html_content': '<strong>This is bold text</strong>'
+    }
+
+    return render(request, 'about.html', data)
 
 def contact(request):
     return HttpResponse("This is the Contact Page")
